2d1874b38d901af1abb3627435047a8da9ca1a1c
Anne <anneguo3@gmail.com>
2020-06-04 20:11:02 -0400

    Created the component for add item

--- budget-buddy/src/components/Add/index.js
+++ budget-buddy/src/components/Add/index.js
@@ -7,28 +7,46 @@ import {
   InputAdornment,
   InputLabel,
   Input,
+  FormControl,
+  Box,
+  Button,
 } from "@material-ui/core";
 
 export default class AddMessage extends React.Component {
   componentDidMount() {
     //
   }
+
   render() {
     return (
-      // add code here
-      <div className="addForm">
-        <form>
+      <div>
+        <Box
+          border={1}
+          m={5}
+          p={2}
+          display="flex"
+          flexDirection="row"
+          justifyContent="space-evenly"
+        >
           <ToggleButtonGroup className="addFormItem">
             <ToggleButton>Expense</ToggleButton>
             <ToggleButton>Income</ToggleButton>
           </ToggleButtonGroup>
-          <InputLabel>Name</InputLabel>
-          <Input />
-          <InputLabel htmlFor="standard-adornment-amount">Amount</InputLabel>
-          <Input
-            id="standard-adornment-amount"
-            startAdornment={<InputAdornment position="start">$</InputAdornment>}
-          />{" "}
+
+          <FormControl className="addFormItem">
+            <InputLabel>Name</InputLabel>
+            <Input />
+          </FormControl>
+
+          <FormControl className="addFormItem">
+            <InputLabel htmlFor="standard-adornment-amount">Amount</InputLabel>
+            <Input
+              id="standard-adornment-amount"
+              startAdornment={
+                <InputAdornment position="start">$</InputAdornment>
+              }
+            />{" "}
+          </FormControl>
           <TextField
             id="date"
             label="Purchase Date"
@@ -39,7 +57,8 @@ export default class AddMessage extends React.Component {
             }}
             className="addFormItem"
           />
-        </form>
+          <Button variant="outlined">Submit Entry</Button>
+        </Box>
       </div>
     );
   }
