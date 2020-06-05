68a82d08d2714e8e817b224eeb538b400eefdbd9
Ansel <anselhartanto@gmail.com>
2020-06-04 21:47:20 -0700

    Integrate Redux and initialState, add color conditional

--- budget-buddy/src/App.js
+++ budget-buddy/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import AddMessage from "./components/Add/index";
-import EntryList from "./components/EntryList/index";
+import {EntryList} from "./components/EntryList/index";
 import Textbox from "./components/Textbox";
 
 function App() {
