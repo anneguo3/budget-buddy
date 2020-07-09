49c6a8adc7be4fbc630fbc850b996115aba68c72
Anne <anneguo3@gmail.com>
2020-07-09 10:53:34 -0400

    Renamed frontend folder

--- budget-buddy/initMongo.js
+++
@@ -1,6 +0,0 @@
-db = db.getSiblingDB("transactionDB");
-db.messages.insertMany([
-    { id: 1, date: Date("2018-07-14") , name: "Purchased Item", price: '10.00', isMoneyIncrease: false },
-    { id: 2, date: Date("2017-08-14"), name: "Car Loan Payment", price: '25.00', isMoneyIncrease: false  },
-    { id: 3, date: Date("2019-03-14"), name: "Payroll", price: '511.00', isMoneyIncrease: true }
-])
