742b764841da576eae549640b5c4695297fcbab6
Anne Guo <anneguo3@gmail.com>
2020-07-24 22:09:04 -0400

    Added front end of the profile customization page

--- frontend/src/components/ProfilePage/index.js
+++ frontend/src/components/ProfilePage/index.js
@@ -1,11 +1,122 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import IconButton from "@material-ui/core/IconButton";
+import DeleteIcon from "@material-ui/icons/Delete";
+import { Box, FormControl, InputLabel, Input, Button } from "@material-ui/core";
+import Typography from "@material-ui/core/Typography";
 
-export default class index extends Component {
-    render() {
-        return (
-            <div>
-                profile page homies
-            </div>
-        )
-    }
+const incomeCategories = ["Chequing", "Savings"];
+
+const expenseCategories = [
+  "Entertainment",
+  "Groceries",
+  "Restaurants",
+  "Housing",
+  "Miscellaneous",
+];
+
+export default class Index extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      incomeCategories: incomeCategories,
+      expenseCategories: expenseCategories,
+    };
+  }
+
+  render() {
+    let incomeList = (
+      <div>
+        <Typography variant="h6">Current Income Categories</Typography>
+        <List component="nav" aria-label="list of income categories">
+          <div>
+            {this.state.incomeCategories.map((category) => (
+              <div style={{ backgroundColor: "rgb(184, 255, 201)" }}>
+                <ListItem button>
+                  <ListItemText id="category" primary={category}></ListItemText>
+                  <IconButton value={category} edge="end" aria-label="delete">
+                    <DeleteIcon />
+                  </IconButton>
+                </ListItem>
+              </div>
+            ))}
+          </div>
+        </List>
+      </div>
+    );
+
+    let addIncome = (
+      <Box display="flex" flexDirection="column" justifyContent="centre">
+        <FormControl>
+          <InputLabel>Income Category</InputLabel>
+          <Input />
+        </FormControl>
+        <Box m={2}>
+          <Button variant="outlined"> Add Income Category</Button>
+        </Box>
+      </Box>
+    );
+
+    let expenseList = (
+      <div>
+        <Typography variant="h6">Current Expense Categories</Typography>
+        <List
+          class="list-item"
+          component="nav"
+          aria-label="list of income categories"
+        >
+          <div>
+            {this.state.expenseCategories.map((category) => (
+              <div style={{ backgroundColor: "rgb(255, 153, 153)" }}>
+                <ListItem button>
+                  <ListItemText id="category" primary={category}></ListItemText>
+                  <IconButton value={category} edge="end" aria-label="delete">
+                    <DeleteIcon />
+                  </IconButton>
+                </ListItem>
+              </div>
+            ))}
+          </div>
+        </List>
+      </div>
+    );
+
+    let addExpense = (
+      <Box display="flex" flexDirection="column" justifyContent="centre">
+        <FormControl>
+          <InputLabel>Expense Category</InputLabel>
+          <Input />
+        </FormControl>
+        <Box m={2}>
+          <Button variant="outlined"> Add Expense Category</Button>
+        </Box>
+      </Box>
+    );
+
+    return (
+      <div>
+        <Box
+          m={5}
+          display="flex"
+          flexDirection="row"
+          justifyContent="space-around"
+        >
+          {incomeList}
+          {addIncome}
+        </Box>
+        <Box
+          m={5}
+          display="flex"
+          flexDirection="row"
+          justifyContent="space-around"
+        >
+          {expenseList}
+          {addExpense}
+        </Box>
+      </div>
+    );
+    // return incomeCategories;
+  }
 }
--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import aggregateReducer from "./aggregateReducer";
 import entriesReducer from "./entriesReducer";
+import profileReducer from "./profileReducer";
 
 export default combineReducers({
   aggregateReducer,
   entriesReducer,
+  profileReducer,
 });
